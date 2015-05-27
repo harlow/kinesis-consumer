8277ffc6be9f8416ac4665d96c3abfab416ac922
Harlow Ward <harlow@hward.com>
2015-05-26 18:50:34 -0700

    Use GoKit logger interface directly

--- logger.go
+++ logger.go
@@ -6,17 +6,11 @@ import (
 	"github.com/go-kit/kit/log"
 )
 
-// Logger sends pipeline info and errors to logging endpoint. The logger could be
-// used to send to STDOUT, Syslog, or any number of distributed log collecting platforms.
-type Logger interface {
-	Log(keyvals ...interface{}) error
-}
-
 // SetLogger adds the ability to change the logger so that external packages
 // can control the logging for this package
-func SetLogger(l Logger) {
+func SetLogger(l log.Logger) {
 	logger = l
 }
 
 // specify a default logger so that we don't end up with panics.
-var logger Logger = log.NewPrefixLogger(os.Stderr)
+var logger log.Logger = log.NewPrefixLogger(os.Stderr)
