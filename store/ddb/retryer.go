9ca79b145c23957b8b1ee24c9428963e6078d6fa
Dimas Yudha P <dimas.yudha@ovo.id>
2019-09-01 05:48:51 +0700

    update readme and fix issue found by linter

--- store/ddb/retryer.go
+++ store/ddb/retryer.go
@@ -10,10 +10,12 @@ type Retryer interface {
 	ShouldRetry(error) bool
 }
 
+// DefaultRetryer .
 type DefaultRetryer struct {
 	Retryer
 }
 
+// ShouldRetry when error occured
 func (r *DefaultRetryer) ShouldRetry(err error) bool {
 	if awsErr, ok := err.(awserr.Error); ok {
 		if awsErr.Code() == dynamodb.ErrCodeProvisionedThroughputExceededException {
